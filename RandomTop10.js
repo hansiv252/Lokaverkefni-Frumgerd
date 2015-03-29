d762be5c4bfa9aae2ba64673ffd3d04714f7dafc
Hansi <HJIvars@hotmail.com>
2015-03-29 16:44:51 +0000

    Revert "useless commit"
    This reverts commit 1fdc668082d7cc0733f56d55697420c6d21d7684.

--- RandomTop10.js
+++ RandomTop10.js
@@ -1,12 +1,5 @@
 var mobileAttacks = [
-    ["M1: Weak Server Side Controls", "https://www.owasp.org/index.php/Mobile_Top_10_2014-M1"],
-    ["M2: Insecure Data Storage", "https://www.owasp.org/index.php/Mobile_Top_10_2014-M2"],
-    ["M3: Insufficient Transport Layer Protection", "https://www.owasp.org/index.php/Mobile_Top_10_2014-M3"],
-    ["M4: Unintended Data Leakage", "https://www.owasp.org/index.php/Mobile_Top_10_2014-M4"],
-    ["M5: Poor Authorization and Authentication", "https://www.owasp.org/index.php/Mobile_Top_10_2014-M5"],
-    ["M6: Broken Cryptography", "https://www.owasp.org/index.php/Mobile_Top_10_2014-M6"],
-    ["M7: Client Side Injection", "https://www.owasp.org/index.php/Mobile_Top_10_2014-M7"],
-    ["M8: Security Decisions Via Untrusted Inputs", "https://www.owasp.org/index.php/Mobile_Top_10_2014-M8"],
-    ["M9: Improper Session Handling", "https://www.owasp.org/index.php/Mobile_Top_10_2014-M9"],
-    ["M10: Lack of Binary Protections", "https://www.owasp.org/index.php/Mobile_Top_10_2014-M10"]
+    ["", ""],
+    ["", ""],
+    ["", ""],
 ];
